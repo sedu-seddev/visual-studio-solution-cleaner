1e4b02579dab09d202bac56338563fc91e6fdbbc
Sebastian Sauter <sebastian.sauter@seddev.de>
2022-09-22 23:16:43 +0200

    Initial version.

---
+++ Sources/Seddev.VisualStudio.SolutionCleaner/SolutionCleanCommand.cs
@@ -0,0 +1,131 @@
+﻿namespace Seddev.VisualStudio.SolutionCleaner
+{
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+    using System;
+    using System.ComponentModel.Design;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using Task = System.Threading.Tasks.Task;
+
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class SolutionCleanCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7a81dddc-354a-43e7-b698-dfc0b41737b8");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SolutionCleanCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private SolutionCleanCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static SolutionCleanCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in SolutionCleanCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new SolutionCleanCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                var solutionDirectory = Directory.GetCurrentDirectory();
+                var projectPaths = Directory.GetFiles(solutionDirectory, "*.csproj", SearchOption.AllDirectories);
+
+                foreach (var projectPath in projectPaths)
+                {
+                    var fileInfo = new FileInfo(projectPath);
+                    var binDirectory = Path.Combine(fileInfo.DirectoryName, "bin");
+                    var objDirectory = Path.Combine(fileInfo.DirectoryName, "obj");
+
+                    if (Directory.Exists(binDirectory))
+                    {
+                        Directory.Delete(binDirectory, true);
+                    }
+
+                    if (Directory.Exists(objDirectory))
+                    {
+                        Directory.Delete(objDirectory, true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var message = $"Error while cleaning the bin and obj folders:{Environment.NewLine}{Environment.NewLine}{ex.Message}{Environment.NewLine}{ex.StackTrace}";
+                var title = "Error";
+
+                // Show a message box to prove we were here
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    message,
+                    title,
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
